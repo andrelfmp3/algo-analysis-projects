f9c4e052600e6ee94a6018a0714496c0736103f4
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2024-04-17 13:49:00 -0300

    meti umas função aí, benção

--- threesum.c
+++ threesum.c
@@ -18,24 +18,69 @@ void treeSumMelhorado(int A[], int n) {
 /* BuscaBinaria(): */
 int BuscaBinaria (int x, int A[], int inicio, int fim)
 {
+    if (inicio <= fim) {
+        int meio = (inicio + fim) / 2; 
 
+        if (A[meio] == x) {
+            return meio; 
+        } else if (A[meio] < x) {
+            return BuscaBinaria(x, A, meio + 1, fim); 
+        } else {
+            return BuscaBinaria(x, A, inicio, meio - 1); 
+        }
+    }
+
+    return -1; 
 }
 
 /* MergeSortRecursivo(): */
 void MergeSortRecursivo(int A[], int inicio, int fim, int n)
 {
-
+    if (inicio < fim) {
+        int meio = (inicio + fim) / 2;
+        ImprimeArray(A, "Array Desord. -" , n);
+        MergeSortRecursivo(A, inicio, meio, n);
+        MergeSortRecursivo(A, meio+1, fim, n);
+        IntercalaSemSentinela(A, inicio, meio, fim, n);
+        ImprimeArray(A, "Array Ord. -", n);
+    }
 }
 
 /* IntercalaSemSentinela(): */
 void IntercalaSemSentinela(int A[], int inicio, int meio, int fim, int n) {
+    int B[fim+1];
+
+    for (int i = inicio; i <= meio; i++) {
+       B[i] = A[i]; 
+    }
+
+    for (int j = meio+1; j <= fim; j++) {
+        B[fim+meio+1-j] = A[j];
+    }
+
+    int i = inicio;
+    int j = meio;
+
+    for (int k = inicio; k <= fim; k++) {
+        if (B[i] <= B[j]) {
+            A[k] = B[i];
+            i++;
+        } else {
+            A[k] = B[j];
+            j--;
+        }
+    }
 
 }
 
 /* ImprimeArray(): */
 void ImprimeArray(int A[], char Msg[], int n)
 {
+    printf("\n%s", Msg);
 
+    for(int i = 0; i < n; i++) {
+        printf(" %d ", A[i]);
+    }
 }
 
 /* ImprimeQtdOperacoes():  */
