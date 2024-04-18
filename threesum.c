295358328b651a06221cdbceecde17f4d33cdf10
Carolvic16 <victoriacarolina.f.silva@gmail.com>
2024-04-17 21:04:29 -0300

    Update threesum.c

--- threesum.c
+++ threesum.c
@@ -11,11 +11,8 @@ void treeSumForcaBruta(int A[], int n) {
     int sum;
 
     for(int i = 0; i < n-2; i ++){
-
         for(int j = i +1; j < n - 1; j++){
-
             for(int k = j+1; k < n; k++){
-
             }
         }
     }
